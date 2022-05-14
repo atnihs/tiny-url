0012458f123549b67f216054d62df42c3369ceac
Khuong Pham <>
2022-05-14 13:34:25 +0700

    Fix to variable userController

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -6,9 +6,8 @@ export const registerEmail = async (req: Request, res: Response) => {
   const { email } = req.body;
   const api_key = nanoid(20);
 
-  const result = await userService.queryAddEmail(email, api_key);
-  // console.log(result);
-  if (!result) {
+  const isAddEmail = await userService.queryAddEmail(email, api_key);
+  if (!isAddEmail) {
     res.status(200).json({
       message: "User exists",
     });
@@ -17,26 +16,4 @@ export const registerEmail = async (req: Request, res: Response) => {
       data: { email, api_key },
     });
   }
-
-  // if (!result) {
-  //   res.status(404).json({ message: "User exists!" });
-  // } else {
-  //   res.status(200).json({ data: { email, api_key } });
-  // }
-
-  // const isEmailExisted = await userService.checkEmailExists(email);
-  // if (!isEmailExisted) {
-  //   userService.queryRegister(email, api_key);
-  //   res.status(200).json({
-  //     message: "success",
-  //     data: {
-  //       email,
-  //       api_key,
-  //     },
-  //   });
-  // } else {
-  //   res.status(200).json({
-  //     message: "Users exist!",
-  //   });
-  // }
 };
